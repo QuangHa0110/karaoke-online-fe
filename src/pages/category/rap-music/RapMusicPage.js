ac5c49324643faa03f97e264abee8e1df9c8f233
QuangHa0110 <quanghadang08@gmail.com>
2023-01-05 20:56:48 +0700

    [CODE] update rap music page

---
+++ src/pages/category/rap-music/RapMusicPage.js
@@ -0,0 +1,71 @@
+/* eslint-disable no-unused-vars */
+import { Card, List, Pagination } from 'antd'
+import SearchForm from 'components/SearchForm/SearchForm'
+import SongItem from 'components/SongItem/SongItem'
+import React from 'react'
+
+// Nhạc rap
+const RapMusic = () => {
+  const data = [
+    {
+      imgLink: '../resources/images/sliders/danh-mat-em.jpg',
+      name: 'Đánh mất em - Karaoke',
+      singer: 'Quang Đăng Trần',
+    },
+    {
+      imgLink: '../resources/images/sliders/danh-mat-em.jpg',
+      name: 'Đánh mất em - Karaoke',
+      singer: 'Quang Đăng Trần',
+    },
+    {
+      imgLink: '../resources/images/sliders/danh-mat-em.jpg',
+      name: 'Đánh mất em - Karaoke',
+      singer: 'Quang Đăng Trần',
+    },
+    {
+      imgLink: '../resources/images/sliders/danh-mat-em.jpg',
+      name: 'Đánh mất em - Karaoke',
+      singer: 'Quang Đăng Trần',
+    },
+    {
+      imgLink: '../resources/images/sliders/danh-mat-em.jpg',
+      name: 'Đánh mất em - Karaoke',
+      singer: 'Quang Đăng Trần',
+    },
+    {
+      imgLink: '../resources/images/sliders/danh-mat-em.jpg',
+      name: 'Đánh mất em - Karaoke',
+      singer: 'Quang Đăng Trần',
+    },
+  ]
+  return (
+    <>
+      <SearchForm />
+      <div style={{ width: '80%', margin: 'auto' }}>
+        <Card title={<h3 style={{ fontWeight: 'bold' }}>NHẠC RAP KARAOKE</h3>}>
+          <List
+            grid={{
+              gutter: 16,
+              xs: 1,
+              sm: 2,
+              md: 4,
+              lg: 4,
+              xl: 6,
+              xxl: 4,
+            }}
+            dataSource={data}
+            renderItem={(item) => (
+              <List.Item>
+                <SongItem item={item} />
+              </List.Item>
+            )}
+          />
+        </Card>
+        <br />
+        <Pagination style={{ textAlign: 'center' }} defaultCurrent={1} total={50} />;
+      </div>
+    </>
+  )
+}
+
+export default RapMusic
