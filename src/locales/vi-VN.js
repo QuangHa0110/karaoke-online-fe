58781a51a84a43be193438f2a62a03211893f978
QuangHa0110 <quanghadang08@gmail.com>
2023-01-09 17:25:04 +0700

    [CODE] add locale vi VN

---
+++ src/locales/vi-VN.js
@@ -0,0 +1,23 @@
+import localeAntd from 'antd/es/locale/vi_VN'
+
+const messages = {
+  'topBar.issuesHistory': 'Issues History',
+  'topBar.projectManagement': 'Project Management',
+  'topBar.typeToSearch': 'Search...',
+  'topBar.findPages': 'Find pages...',
+  'topBar.actions': 'Actions',
+  'topBar.status': 'Status',
+  'topBar.profileMenu.hello': 'Xin chào',
+  'topBar.profileMenu.billingPlan': 'Billing Plan',
+  'topBar.profileMenu.role': 'Vai trò',
+  'topBar.profileMenu.email': 'Email',
+  'topBar.profileMenu.phone': 'SĐT',
+  'topBar.profileMenu.editProfile': 'Thông tin cá nhân',
+  'topBar.profileMenu.logout': 'Đăng xuất',
+}
+
+export default {
+  locale: 'vi-VN',
+  localeAntd,
+  messages,
+}
