18b130e0e25c15989a4eedfc01b9b8f71223cce8
vuduy-2k3 <vdduy020320@gmail.com>
2023-01-11 09:30:51 +0000

    update redux

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -4,12 +4,14 @@ import user from './user/reducers'
 import menu from './menu/reducers'
 import settings from './settings/reducers'
 import singer from './singer/reducers'
+import song from './song/reducers'
 
-export default history =>
+export default (history) =>
   combineReducers({
     router: connectRouter(history),
     user,
     menu,
     settings,
     singer,
+    song,
   })
--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -3,7 +3,8 @@ import user from './user/sagas'
 import menu from './menu/sagas'
 import settings from './settings/sagas'
 import singer from './singer/sagas'
+import song from './song/sagas'
 
 export default function* rootSaga() {
-  yield all([user(), menu(), settings(), singer()])
+  yield all([user(), menu(), settings(), singer(), song()])
 }
