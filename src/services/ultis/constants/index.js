5db8aa85e1211bb410a747d3b095f50022e7bf31
QuangHa0110 <quanghadang08@gmail.com>
2023-01-11 20:59:20 +0700

    [CODE] update constants

--- src/services/ultis/constants/index.js
+++ src/services/ultis/constants/index.js
@@ -4,3 +4,11 @@ export const BASE_URL_SERVER = 'http://localhost:1337'
 export const DEFAULT_PAGE = 1
 
 export const DEFAULT_PAGE_SIZE = 20
+
+export const MUSIC_GENRE= {
+    YOUNG_MUSIC: 'YOUNG_MUSIC',
+    RAP_MUSIC: 'RAP_MUSIC',
+    CHILDREN_MUSIC: 'CHILDREN_MUSIC',
+    LYRICAL_MUSIC: 'LYRICAL_MUSIC',
+    CHRISTMAS_MUSIC: 'CHRISTMAS_MUSIC',
+}
